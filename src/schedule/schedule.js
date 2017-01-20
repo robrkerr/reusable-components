eca0ee5b95ed2c36c14fd4be4c673ad8004b2e02
Robert Kerr <robrkerr@gmail.com>
2017-01-20 12:26:23 +1000

    Added more dummy content.

--- src/schedule/schedule.js
+++ src/schedule/schedule.js
@@ -27,25 +27,29 @@ export default [
     title: "\"Advanced Fluid Typography (and more)\"",
     presenter: "Mike Riethmuller",
     start: "11:20",
-    finish: "11:40"
+    finish: "11:40",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id convallis nibh. In hac habitasse platea dictumst. Vivamus nulla neque, tempor ut cursus vel, tristique at lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus vitae est finibus, dictum metus et, laoreet orci. Proin pharetra, mauris id mattis varius, risus lectus luctus justo, et varius ex elit vitae neque. Vestibulum eget magna eget sem interdum tempor vel in eros. Nunc vitae laoreet nunc. Fusce non ultrices lorem. Pellentesque eget risus id felis semper fringilla sit amet sed nisl. Fusce lobortis eget metus in eleifend. Sed gravida enim et sapien laoreet, vel pretium sem molestie."
   }, {
     type: "talk",
     title: "\"Enhancing Mobile Experiences with Device APIs\"",
     presenter: "Jessica Claire Edwards",
     start: "11:40",
-    finish: "12:00"
+    finish: "12:00",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id convallis nibh. In hac habitasse platea dictumst. Vivamus nulla neque, tempor ut cursus vel, tristique at lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus vitae est finibus, dictum metus et, laoreet orci. Proin pharetra, mauris id mattis varius, risus lectus luctus justo, et varius ex elit vitae neque. Vestibulum eget magna eget sem interdum tempor vel in eros. Nunc vitae laoreet nunc. Fusce non ultrices lorem. Pellentesque eget risus id felis semper fringilla sit amet sed nisl. Fusce lobortis eget metus in eleifend. Sed gravida enim et sapien laoreet, vel pretium sem molestie."
   }, {
     type: "talk",
     title: "\"Dude, where's my Tricorder\"",
     presenter: "Laura Summers",
     start: "12:00",
-    finish: "12:20"
+    finish: "12:20",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id convallis nibh. In hac habitasse platea dictumst. Vivamus nulla neque, tempor ut cursus vel, tristique at lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus vitae est finibus, dictum metus et, laoreet orci. Proin pharetra, mauris id mattis varius, risus lectus luctus justo, et varius ex elit vitae neque. Vestibulum eget magna eget sem interdum tempor vel in eros. Nunc vitae laoreet nunc. Fusce non ultrices lorem. Pellentesque eget risus id felis semper fringilla sit amet sed nisl. Fusce lobortis eget metus in eleifend. Sed gravida enim et sapien laoreet, vel pretium sem molestie."
   }, {
     type: "talk",
     title: "\"Elm as an antidote to front-end fatigue\"",
     presenter: "Francesco Orsenigo",
     start: "12:20",
-    finish: "12:40"
+    finish: "12:40",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id convallis nibh. In hac habitasse platea dictumst. Vivamus nulla neque, tempor ut cursus vel, tristique at lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus vitae est finibus, dictum metus et, laoreet orci. Proin pharetra, mauris id mattis varius, risus lectus luctus justo, et varius ex elit vitae neque. Vestibulum eget magna eget sem interdum tempor vel in eros. Nunc vitae laoreet nunc. Fusce non ultrices lorem. Pellentesque eget risus id felis semper fringilla sit amet sed nisl. Fusce lobortis eget metus in eleifend. Sed gravida enim et sapien laoreet, vel pretium sem molestie."
   }, {
     type: "break",
     title: "Lunch",
@@ -61,31 +65,36 @@ export default [
     title: "\"Mars Needs Guitars.... and the WebAudio API\"",
     presenter: "Cathy Lill",
     start: "14:20",
-    finish: "14:40"
+    finish: "14:40",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id convallis nibh. In hac habitasse platea dictumst. Vivamus nulla neque, tempor ut cursus vel, tristique at lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus vitae est finibus, dictum metus et, laoreet orci. Proin pharetra, mauris id mattis varius, risus lectus luctus justo, et varius ex elit vitae neque. Vestibulum eget magna eget sem interdum tempor vel in eros. Nunc vitae laoreet nunc. Fusce non ultrices lorem. Pellentesque eget risus id felis semper fringilla sit amet sed nisl. Fusce lobortis eget metus in eleifend. Sed gravida enim et sapien laoreet, vel pretium sem molestie."
   }, {
     type: "talk",
     title: "\"The Promised Path to Async, Awaits\"",
     presenter: "Michael Robinson",
     start: "14:40",
-    finish: "15:00"
+    finish: "15:00",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id convallis nibh. In hac habitasse platea dictumst. Vivamus nulla neque, tempor ut cursus vel, tristique at lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus vitae est finibus, dictum metus et, laoreet orci. Proin pharetra, mauris id mattis varius, risus lectus luctus justo, et varius ex elit vitae neque. Vestibulum eget magna eget sem interdum tempor vel in eros. Nunc vitae laoreet nunc. Fusce non ultrices lorem. Pellentesque eget risus id felis semper fringilla sit amet sed nisl. Fusce lobortis eget metus in eleifend. Sed gravida enim et sapien laoreet, vel pretium sem molestie."
   }, {
     type: "talk",
     title: "\"Gradient Circus\"",
     presenter: "Gilmore Davidson",
     start: "15:00",
-    finish: "15:20"
+    finish: "15:20",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id convallis nibh. In hac habitasse platea dictumst. Vivamus nulla neque, tempor ut cursus vel, tristique at lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus vitae est finibus, dictum metus et, laoreet orci. Proin pharetra, mauris id mattis varius, risus lectus luctus justo, et varius ex elit vitae neque. Vestibulum eget magna eget sem interdum tempor vel in eros. Nunc vitae laoreet nunc. Fusce non ultrices lorem. Pellentesque eget risus id felis semper fringilla sit amet sed nisl. Fusce lobortis eget metus in eleifend. Sed gravida enim et sapien laoreet, vel pretium sem molestie."
   }, {
     type: "talk",
     title: "\"The future of communication on the web\"",
     presenter: "Adam Ullman",
     start: "15:20",
-    finish: "15:40"
+    finish: "15:40",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id convallis nibh. In hac habitasse platea dictumst. Vivamus nulla neque, tempor ut cursus vel, tristique at lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus vitae est finibus, dictum metus et, laoreet orci. Proin pharetra, mauris id mattis varius, risus lectus luctus justo, et varius ex elit vitae neque. Vestibulum eget magna eget sem interdum tempor vel in eros. Nunc vitae laoreet nunc. Fusce non ultrices lorem. Pellentesque eget risus id felis semper fringilla sit amet sed nisl. Fusce lobortis eget metus in eleifend. Sed gravida enim et sapien laoreet, vel pretium sem molestie."
   }, {
     type: "talk",
     title: "\"The JS community and sociolinguistics: A love story\"",
     presenter: "Harriet Lawrence",
     start: "15:40",
-    finish: "16:00"
+    finish: "16:00",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id convallis nibh. In hac habitasse platea dictumst. Vivamus nulla neque, tempor ut cursus vel, tristique at lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus vitae est finibus, dictum metus et, laoreet orci. Proin pharetra, mauris id mattis varius, risus lectus luctus justo, et varius ex elit vitae neque. Vestibulum eget magna eget sem interdum tempor vel in eros. Nunc vitae laoreet nunc. Fusce non ultrices lorem. Pellentesque eget risus id felis semper fringilla sit amet sed nisl. Fusce lobortis eget metus in eleifend. Sed gravida enim et sapien laoreet, vel pretium sem molestie."
   }, {
     type: "break",
     title: "Break",
